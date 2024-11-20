d386d9450aee535bc98e202645dad351142ba400
Cameron <camerong888@gmail.com>
2024-11-20 14:19:13 -0500

    removed unecessary functions. Will update this

--- vision/apriltag.py
+++ vision/apriltag.py
@@ -7,18 +7,7 @@ import tf2_geometry_msgs
 from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import PoseStamped
 import yaml
-
-
-# loading the parameters of kinova camera
-def load_camera_parameters():
-    camera_params = {
-        "fx": 1297.672904,
-        "fy": 1298.631344,
-        "cx": 620.914026,
-        "cy": 238.280325,
-        "distortion": [0.0, 0.0, 0.0, 0.0, 0.0]
-    }
-    return camera_params
+from tf2_ros import TransformStamped
 
 class AprilTagProcessor:
     # initialize ros parameters as apriltag_tf_processor and buffer for tf2 transform
@@ -29,52 +18,6 @@ class AprilTagProcessor:
         self.broadcaster = tf2_ros.TransformBroadcaster()
         self.static_broadcaster = tf2_ros.StaticTransformBroadcaster()
 
-        # subscribe AprilTag detection
-        rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.apriltag_callback)
-
-    def apriltag_callback(self, data):
-        """
-        AprilTag detection and update TF
-        """
-        if len(data.detections) > 0:
-            # The first detected as the goal
-            tag = data.detections[0]
-            tag_pose = tag.pose.pose.pose
-
-            # publish `apriltag_arm` frame
-            self.broadcast_transform(
-                parent_frame="arm_camera_frame",
-                child_frame="apriltag_arm",
-                pose=tag_pose
-            )
-
-            # publish `apriltag_realsense` frame
-            self.broadcast_transform(
-                parent_frame="realsense_camera_frame",
-                child_frame="apriltag_realsense",
-                pose=tag_pose
-            )
-
-            # align frames
-            self.align_apriltag_frames()
-
-            # broadcase Realsense Depth Camera static transform
-            self.broadcast_realsense_depth_camera()
-
-    def broadcast_transform(self, parent_frame, child_frame, pose):
-        """
-        publish Transform
-        """
-        transform = TransformStamped()
-        transform.header.stamp = rospy.Time.now()
-        transform.header.frame_id = parent_frame
-        transform.child_frame_id = child_frame
-        transform.transform.translation.x = pose.position.x
-        transform.transform.translation.y = pose.position.y
-        transform.transform.translation.z = pose.position.z
-        transform.transform.rotation = pose.orientation
-
-        self.broadcaster.sendTransform(transform)
 
     def align_apriltag_frames(self):
         """
@@ -91,7 +34,7 @@ class AprilTagProcessor:
                 parent_frame="apriltag_realsense",
                 child_frame="aligned_apriltag_frame",
                 translation=[0.0, 0.0, 0.0],
-                rotation=[0.0, 0.0, 0.0, 1.0]  # 单位四元数
+                rotation=[0.0, 0.0, 0.0, 1.0]  # unit quaternion
             )
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rospy.logwarn("Transform lookup failed for aligning AprilTag frames.")
